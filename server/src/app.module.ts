760cbd3e0dbbee011924fc9f57de964eb801607c
hisato yoneda <h.yoneda@day-1.jp>
2022-09-06 17:28:46 +0900

    add: app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ApiModule } from './api/api.module'
+import { CrawlerModule } from './crawler/crawler.module'
+import { DbModule } from './db/db.module'
+import { TaskModule } from './task/task.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ApiModule,
+    CrawlerModule,
+    DbModule,
+    TaskModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
